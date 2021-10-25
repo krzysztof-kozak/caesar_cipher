d994cb0821242fea87b9f79f370840a2be90d36e
Kris <krzysztofkoz6@gmail.com>
2021-10-25 10:36:12 +0200

    Implement caesar13 function

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import ceasar13 from "./modules/caesar13";
+
+console.log(ceasar13("___PRZEPROGRAMOWANI...,  1337"));
--- src/modules/caesar13.js
+++ src/modules/caesar13.js
@@ -6,6 +6,20 @@ function ceasar13(message) {
   if (message.length === 0) {
     return `Empty strings are not allowed.`;
   }
+
+  const alphabet = createAlphabet();
+  const shift = 13;
+
+  let encryptedMessage = "";
+  for (const letter of message) {
+    if (!alphabet.includes(letter.toLowerCase())) {
+      encryptedMessage += letter;
+    } else {
+      encryptedMessage += shiftLetter(letter, alphabet, shift);
+    }
+  }
+
+  return encryptedMessage;
 }
 
 function createAlphabet() {
@@ -18,6 +32,15 @@ function createAlphabet() {
   }
 
   return alphabet;
+}
+
+function shiftLetter(letter, alphabet, shift) {
+  const isUpperCase = letter === letter.toUpperCase();
+
+  const index = alphabet.indexOf(letter.toLowerCase());
+  const shiftedLetter = alphabet[(index + shift) % 26];
+
+  return isUpperCase ? shiftedLetter.toUpperCase() : shiftedLetter;
 }
 
 export default ceasar13;
